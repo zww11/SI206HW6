120a2c8076f834007906252a2c8872b3977609c7
Barbara Ericson <43884467+barbarer@users.noreply.github.com>
2018-10-16 15:45:35 -0400

    Add files via upload
    Added files

---
+++ hw6.py
@@ -0,0 +1,34 @@
+import re
+import unittest
+
+def sumNums(fileName):
+    pass
+
+def countWord(fileName, word):
+    pass
+
+def listURLs(fileName):
+    pass
+
+
+class TestHW6(unittest.TestCase):
+    """ Class to test this homework """
+
+    def test_sumNums1(self):
+        """ test sumNums on the first file """
+        self.assertEqual(sumNums("regex_sum_42.txt"), 445833)
+
+    def test_sumNums2(self):
+        """ test sumNums on the second file """
+        self.assertEqual(sumNums("regex_sum_132198.txt"), 374566)
+
+    def test_countWord(self):
+        """ test count word on the first file """
+        self.assertEqual(countWord("regex_sum_42.txt", "computer"),15)
+
+    def test_listURLs(self):
+        """ test list URLs on the first file """
+        self.assertEqual(len(listURLs("regex_sum_42.txt")), 3)
+
+# run the tests
+unittest.main(verbosity=2)
